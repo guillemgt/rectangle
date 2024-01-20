8150e9c9b926055452862e522567f80ff2fe0594
guillemgt <ggarciatarrach@gmail.com>
2024-01-20 09:52:19 +0000

    Updated grid generator to find best letters to remove

--- generator/grids.py
+++ generator/grids.py
@@ -43,38 +43,44 @@ class Trie:
         if char in node.children:
             yield from self.get_next_chars(word[1:], node.children[char])
 
-def fill_grid(hor_trie_grid: list[list[TrieNode]], ver_trie_grid: list[list[TrieNode]], letter_grid, row_trie, current_index):
+def fill_grid(hor_trie_grid: list[list[TrieNode]], ver_trie_grid: list[list[TrieNode]], letter_grid, current_index):
     if current_index[0] == grid_size[0]:
         yield letter_grid
         return
 
     row_index, col_index = current_index
 
-    left_node = hor_trie_grid[row_index][col_index - 1] if col_index > 0 else row_trie.root
-    up_node = ver_trie_grid[row_index - 1][col_index]
+    left_node = hor_trie_grid[row_index][col_index]
+    up_node = ver_trie_grid[row_index][col_index]
 
     possibilities = Counter([c for c in left_node.children.keys()]) & Counter([c for c in up_node.children.keys()])
 
     if current_index == (len(letter_grid) - 1, len(letter_grid[0]) - 1):
 
         for char in possibilities:
-            hor_trie_grid[row_index][col_index] = left_node.children[char]
-            ver_trie_grid[row_index][col_index] = up_node.children[char]
+            if left_node.children[char].frequency == 0 or up_node.children[char].frequency == 0:
+                continue
+
             letter_grid[row_index][col_index] = char
-            freq_sum = sum(hor_trie_grid[i][col_index].frequency for i in range(grid_size[0])) + sum(ver_trie_grid[row_index][i].frequency for i in range(grid_size[1]))
+            freq_sum = sum(hor_trie_grid[i][col_index].children[letter_grid[i][col_index]].frequency for i in range(grid_size[0])) + sum(ver_trie_grid[row_index][i].children[letter_grid[row_index][i]].frequency for i in range(grid_size[1]))
 
             yield freq_sum, '\n'.join(''.join(row) for row in letter_grid)
 
     else:
 
         for char in possibilities:
-            hor_trie_grid[row_index][col_index] = left_node.children[char]
-            ver_trie_grid[row_index][col_index] = up_node.children[char]
+            if col_index < grid_size[1] - 1:
+                hor_trie_grid[row_index][col_index + 1] = left_node.children[char]
+            elif left_node.children[char].frequency == 0:
+                continue
+            if row_index < grid_size[0] - 1:
+                ver_trie_grid[row_index + 1][col_index] = up_node.children[char]
+            elif up_node.children[char].frequency == 0:
+                continue
+
             letter_grid[row_index][col_index] = char
             next_index = (row_index, col_index + 1) if col_index < grid_size[1] - 1 else (row_index + 1, 0)
-            yield from fill_grid(hor_trie_grid, ver_trie_grid, letter_grid, row_trie, next_index)
-
-
+            yield from fill_grid(hor_trie_grid, ver_trie_grid, letter_grid, next_index)
 
 
 
@@ -87,48 +93,49 @@ word_frequencies = json.load(open('wordsFreq.json'))
 #         print(word, freq)
 #         break
 
-word_frequencies = {word: math.log10(freq) for word, freq in word_frequencies.items() if freq > 1_000_000}
-word_list = list(open('words.txt', 'r', encoding='utf8').read().split('\n'))
-word_frequencies = {word: word_frequencies[word] for word in word_list if word in word_frequencies}
+word_frequencies = {word: math.log10(freq) for word, freq in word_frequencies.items() if freq > 0}
+word_list = list(set(list(open('words.txt', 'r', encoding='utf8').read().split('\n'))))
+print(len(word_list))
+word_list = [w for w in word_list if w in word_frequencies]
+word_frequencies = {word: word_frequencies[word] for word in word_list}
+
+with open('words_processed.txt','w') as file:
+    file.write("\n".join(word_list))
 
 
 output = {}
 
-max_length = 10
+max_length = max(len(w) for w in word_list)
+
+length_to_words = [[] for i in range(max_length)]
+length_to_trie = [Trie() for i in range(max_length)]
+for i in range(max_length):
+    length_to_words[i] = [w for w in word_list if len(w) == i]
+    for word in length_to_words[i]:
+        length_to_trie[i].insert(word, word_frequencies[word])
+
 
 for x in range(2, max_length):
     for y in range(x, max_length):
         print(x, y)
         grid_size = (x, y)
-        tries = (Trie(), Trie())
-        words = [[], []]
-
-        for axis in range(2):
-            words[axis] = [word for word, freq in word_frequencies.items() if len(word) == grid_size[axis]]
-            trie = tries[axis]
-            for word in words[axis]:
-                freq = word_frequencies[word]
-                trie.insert(word, freq)
+        tries = (length_to_trie[x], length_to_trie[y])
+        words = [length_to_words[x], length_to_words[y]]
 
         found_grids = []
 
-        for first_word in tqdm(words[1]):
-            hor_trie_grid = [[None for _ in range(grid_size[1])] for _ in range(grid_size[0])]
-            hor_trie_grid[0][0] = tries[1].root.children[first_word[0]]
-            for i in range(1, grid_size[1]):
-                hor_trie_grid[0][i] = hor_trie_grid[0][i-1].children.get(first_word[i], None)
-                if hor_trie_grid[0][i] is None:
-                    break
-            ver_trie_grid = [[None for _ in range(grid_size[1])] for _ in range(grid_size[0])]
-            ver_trie_grid[0] = [tries[0].root.children.get(c, None) for c in first_word]
-            if None in ver_trie_grid[0]:
-                continue
-            letter_grid = [[None for _ in range(grid_size[1])] for _ in range(grid_size[0])]
-            letter_grid[0] = [c for c in first_word]
+        hor_trie_grid = [[None for _ in range(grid_size[1])] for _ in range(grid_size[0])]
+        for i in range(grid_size[0]):
+            hor_trie_grid[i][0] = tries[1].root
 
-            for result in fill_grid(hor_trie_grid, ver_trie_grid, letter_grid, tries[1], (1, 0)):
-                found_grids.append(result)
+        ver_trie_grid = [[None for _ in range(grid_size[1])] for _ in range(grid_size[0])]
+        for i in range(grid_size[1]):
+            ver_trie_grid[0][i] = tries[0].root
+        
+        letter_grid = [[None for _ in range(grid_size[1])] for _ in range(grid_size[0])]
 
+        for result in fill_grid(hor_trie_grid, ver_trie_grid, letter_grid, (0, 0)):
+            found_grids.append(result)
 
         found_grids = sorted(found_grids, key=lambda x: x[0], reverse=True)
 
--- generator/parse.py
+++ generator/parse.py
@@ -1,6 +1,12 @@
 import json
+import math
+import re
+from tqdm import tqdm
 
-data = json.load(open('grids.json'))
+import concurrent.futures
+import functools
+
+data = json.load(open('generator/grids.json'))
 
 new_data = []
 
@@ -18,10 +24,149 @@ new_data = []
 #         if not symmetric:
 #             new_data.append(el["grid"])
 
+word_frequencies = json.load(open('generator/wordsFreq.json'))
+word_frequencies = {word: math.log10(freq) for word, freq in word_frequencies.items() if freq > 1_000_000}
+word_list = list(open('generator/words.txt', 'r', encoding='utf8').read().split('\n'))
+word_list = [w for w in word_list if w in word_frequencies]
+word_frequencies = {word: word_frequencies[word] for word in word_list}
+
+# entropy_cache = {}
+
+# def get_entropy(word_pattern):
+#     if word_pattern in entropy_cache:
+#         return entropy_cache[word_pattern]
+#     matching_words = [w for w in word_list if re.match(word_pattern, w)]
+#     result = sum([1.0/word_frequencies[w] for w in matching_words])
+#     entropy_cache[word_pattern] = result
+#     return result
+
+# @functools.lru_cache(maxsize=None)
+# def get_entropy(word_pattern):
+#     matching_words = [w for w in word_list if re.match(word_pattern, w)]
+#     result = sum([1.0/word_frequencies[w] for w in matching_words])
+#     return result
+
+# def compute_score(string):
+#     grid = [[c for c in line] for line in string.split("\n")]
+
+#     score = 0
+
+#     letters_to_remove = 8
+
+#     # positions = []
+
+#     while letters_to_remove > 0:
+#         letters_to_remove -= 1
+#         # Find the letter with the lowest entropy
+
+#         best_entropy = float("inf")
+#         best_position = None
+
+#         for i in range(len(grid)):
+#             for j in range(len(grid[i])):
+#                 if grid[i][j] == ".":
+#                     continue
+
+#                 horizontal_word = [c for c in grid[i]]
+#                 horizontal_word[j] = "."
+                
+#                 vertical_word = [grid[k][j] for k in range(len(grid))]
+#                 vertical_word[i] = "."
+
+#                 entropy = get_entropy("".join(horizontal_word)) + get_entropy("".join(vertical_word))
+                
+#                 if entropy < best_entropy:
+#                     best_entropy = entropy
+#                     best_position = (i, j)
+
+#         # Replace the letter with the lowest entropy with a *
+#         i, j = best_position
+#         grid[i][j] = "."
+#         # positions.append(best_position)
+
+#         # Update the score
+#         score += -best_entropy
+
+#     # for k, (i, j) in enumerate(positions):
+#     #     grid[i][j] = str(k)
+
+#     return score, grid
+
+@functools.lru_cache(maxsize=None)
+def get_entropy(word_pattern):
+    matching_words = [w for w in word_list if re.match(word_pattern, w)]
+    return sum([1.0 / word_frequencies[w] for w in matching_words])
+
+def compute_entropy_for_position(args):
+    i, j, grid = args
+    if grid[i][j] == ".":
+        return float("inf"), None
+
+    horizontal_word = [c for c in grid[i]]
+    horizontal_word[j] = "."
+
+    vertical_word = [grid[k][j] for k in range(len(grid))]
+    vertical_word[i] = "."
+
+    entropy = get_entropy("".join(horizontal_word)) + get_entropy("".join(vertical_word))
+    return entropy, (i, j)
+
+def compute_score(string):
+    grid = [[c for c in line] for line in string.split("\n")]
+    score = 0
+    letters_to_remove = 20
+    removed_positions = []
+
+    while letters_to_remove > 0:
+        letters_to_remove -= 1
+        positions = [(i, j, grid) for i in range(len(grid)) for j in range(len(grid[i]))]
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            results = executor.map(compute_entropy_for_position, positions)
+
+        best_entropy, best_position = min(results, key=lambda x: x[0])
+
+        # Replace the letter with the lowest entropy with a '.'
+        if best_position:
+            i, j = best_position
+            grid[i][j] = "."
+            score += best_entropy
+            removed_positions.append(best_position)
+
+    for k, (i, j) in enumerate(removed_positions):
+        grid[i][j] = k
+
+    return score, grid
+
+
 for i in range(4, 10):
-    for j in range(i+1, 10):
+    for j in range(i, 10):
+        if i == j == 4:
+            continue
         key = str((i, j))
-        for el in data[key]:
-            new_data.append(el["grid"])
+        pbar = tqdm(data[key])
+        pbar.set_description(key)
+        for el in pbar:
+            if i == j:
+                grid = el["grid"].split("\n")
+                symmetric_fails = 0
+                for l in range(i):
+                    for k in range(l+1, i):
+                        if grid[l][k] != grid[k][l]:
+                            symmetric_fails += 1
+                if symmetric_fails < i-1:
+                    continue
+
+            score, grid = compute_score(el["grid"])
+            new_data.append({
+                "solution": el["grid"],
+                "order": grid,
+                "difficulty": score
+            })
+            # print(grid)
+
+new_data = sorted(new_data, key=lambda x: x["difficulty"], reverse=True)
+
+print("Found:", len(new_data))
 
 json.dump(new_data, open('games.json', 'w'), indent=1)
